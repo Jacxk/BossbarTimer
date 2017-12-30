dac3ce2d4a0d14bc2cac9308a0335e768f3eef8e
Dustin Utecht <me@dustin-utecht.de>
2017-12-30 02:19:01 +0100

    added messageUtil and cleanup the commands

--- src/com/minestom/Commands/BbtCommand.java
+++ src/com/minestom/Commands/BbtCommand.java
@@ -3,6 +3,7 @@ package com.minestom.Commands;
 import com.minestom.BarInterface.BossbarInterface;
 import com.minestom.BossBarManager;
 import com.minestom.BossbarTimer;
+import com.minestom.Utils.MessageUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -18,18 +19,14 @@ public class BbtCommand implements CommandExecutor {
         this.plugin = plugin;
     }
 
-    private String colorMessage(String text) {
-        return ChatColor.translateAlternateColorCodes('&', text);
-    }
-
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!command.getName().equalsIgnoreCase("bossbartimer") || !(sender instanceof Player)) {
             return true;
         }
+
         Integer argsLength = args.length;
         Player player = (Player) sender;
-
         if (argsLength == 0) {
             if (plugin.editing.contains(player)) {
                 BossbarInterface.createEditMenu(player, plugin);
@@ -37,106 +34,133 @@ public class BbtCommand implements CommandExecutor {
             return true;
         }
 
-        if (args[0].equalsIgnoreCase("help")) {
-            if (argsLength == 1) {
-                player.sendMessage(colorMessage("&8----------------------------"));
-                player.sendMessage(colorMessage("&c&l        BossBarTimer"));
-                player.sendMessage(colorMessage(""));
-                player.sendMessage(colorMessage("&8 - &a/bbt start <name>"));
-                player.sendMessage(colorMessage("&8 - &a/bbt stop <name>"));
-                player.sendMessage(colorMessage("&8 - &a/bbt create <name> <time> <color> <style> <DisplayName>"));
-                player.sendMessage(colorMessage("&8 - &a/bbt reload"));
-                player.sendMessage(colorMessage(""));
-                player.sendMessage(colorMessage("&7TIP: Use 'Tab' to autocomplete some arguments, such as <color>, <style> and <name>"));
-                player.sendMessage(colorMessage(""));
-                player.sendMessage(colorMessage("&8----------------------------"));
-            }
+        switch (args[0]) {
+            case "help":
+                helpParameter(player, argsLength);
+                break;
+            case "reload":
+                reloadParameter(player, argsLength);
+                break;
+            case "stop":
+                stopParameter(player, argsLength, args);
+                break;
+            case "create":
+                createParameter(player, argsLength, args);
+                break;
+            case "start":
+                startParameter(player, argsLength, args);
+                break;
         }
-
-        if (args[0].equalsIgnoreCase("reload")) {
-            if (argsLength == 1) {
-                plugin.reloadConfig();
-                player.sendMessage(colorMessage("&cBossBarTimer > &7The configuration file has been reloaded!"));
-                for (String barName : plugin.getConfig().getConfigurationSection("Bars").getKeys(false)) {
-                    if (!plugin.getBarManagerMap().containsKey(barName)) {
-                        plugin.getBarManagerMap().put(barName, new BossBarManager(plugin));
-                    }
-                }
-            }
-        }
-
-        if (args[0].equalsIgnoreCase("stop")) {
-            if (argsLength == 1) {
-                player.sendMessage(colorMessage("&cBossBarTimer > &7You need to specify a bar!"));
-                return true;
-            }
-            String barName = args[1];
-            BossBarManager bossBar = plugin.getBarManagerMap().get(barName);
-            if (!plugin.getTimer().containsKey(barName)) {
-                player.sendMessage(colorMessage("&cBossBarTimer > &7That bar is not active!"));
-                return true;
-            }
-            for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-                bossBar.removeBar(onlinePlayer);
-            }
-            plugin.getTimer().remove(barName);
-            bossBar.setFinished(false);
-            player.sendMessage(colorMessage("&cBossBarTimer > &7The bar timer has been stopped!"));
-        }
-
-        if (args[0].equalsIgnoreCase("create")) {
-            if (argsLength == 1 || argsLength == 2 || argsLength == 3 || argsLength == 4) {
-                player.sendMessage(colorMessage("&cBossBarTimer > &7Wrong usage! Use: &a/bbt create <name> <time> <color> <style> <DisplayName>"));
-                return true;
-            }
-            String barName = args[1];
-            String time = args[2];
-            String color = args[3];
-            String style = args[4];
-            StringBuilder displayName = new StringBuilder();
-            for (int i = 5; i < argsLength; i++) {
-                displayName.append(args[i] + " ");
-            }
-            plugin.getConfig().set("Bars." + barName + ".DisplayName", displayName.toString().trim());
-            plugin.getConfig().set("Bars." + barName + ".Time", time);
-            plugin.getConfig().set("Bars." + barName + ".Color", color.toUpperCase());
-            plugin.getConfig().set("Bars." + barName + ".Style", style.toUpperCase());
-            plugin.saveConfig();
-            plugin.getBarManagerMap().put(barName, plugin.getBarManager());
-            player.sendMessage(colorMessage("&cBossBarTimer > &7A new bar timer has been created!"));
-        }
-
-        if (args[0].equalsIgnoreCase("start")) {
-            if (argsLength == 1) {
-                player.sendMessage(colorMessage("&cBossBarTimer > &7You need to specify a bar!"));
-                return true;
-            }
-            String barName = args[1];
-            if (!plugin.getConfig().isConfigurationSection("Bars." + barName)) {
-                player.sendMessage(colorMessage("&cBossBarTimer > &7That bar is not available try another one!"));
-                return true;
-            }
-            if (plugin.getTimer().containsKey(barName)) {
-                player.sendMessage(colorMessage("&cBossBarTimer > &7That bar is already active!"));
-                return true;
-            }
-            String timeFormat = plugin.getConfig().getString("Bars." + barName + ".Time");
-            plugin.getUtilities().formatTime(barName, timeFormat);
-            BossBarManager bossBar = plugin.getBarManagerMap().get(barName);
-            for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-                bossBar.addPlayer(onlinePlayer);
-            }
-            bossBar.setBarColor(plugin.getConfig().getString("Bars." + barName + ".Color").toUpperCase());
-            bossBar.setBarStyle(plugin.getConfig().getString("Bars." + barName + ".Style").toUpperCase());
-            bossBar.setFinished(false);
-
-            player.sendMessage(colorMessage("&cBossBarTimer > &7The bar &e" + barName + " &7has been started!"));
-            plugin.getUtilities().animateText(plugin.getConfig().getStringList("Bars." + barName + ".DisplayName.Frames"),
-                    plugin.getConfig().getLong("Bars." + barName + ".DisplayName.Period"), bossBar);
-        }
-
         return true;
     }
 
+    private void helpParameter(Player player, Integer argsLength)
+    {
+        if (argsLength == 1) {
+            String[] messages = {
+                    "&8----------------------------",
+                    "&c&l        BossBarTimer",
+                    "",
+                    "&8 - &a/bbt start <name>",
+                    "&8 - &a/bbt stop <name>",
+                    "&8 - &a/bbt create <name> <time> <color> <style> <DisplayName>",
+                    "&8 - &a/bbt reload",
+                    "",
+                    "&7TIP: Use 'Tab' to autocomplete some arguments, such as <color>, <style> and <name>",
+                    "",
+                    "&8----------------------------"
+            };
+            MessageUtil.sendMessages(player, messages, false);
+        }
+    }
 
+    private void reloadParameter(Player player, Integer argsLength)
+    {
+        if (argsLength == 1) {
+            plugin.reloadConfig();
+            MessageUtil.sendMessage(player, "The configuration file has been reloaded!");
+            for (String barName : plugin.getConfig().getConfigurationSection("Bars").getKeys(false)) {
+                if (!plugin.getBarManagerMap().containsKey(barName)) {
+                    plugin.getBarManagerMap().put(barName, new BossBarManager(plugin));
+                }
+            }
+        }
+    }
+
+    private void stopParameter(Player player, Integer argsLength, String[] args)
+    {
+        if (argsLength == 1) {
+            MessageUtil.sendMessage(player, "You need to specify a bar!");
+            return;
+        }
+        String barName = args[1];
+        BossBarManager bossBar = plugin.getBarManagerMap().get(barName);
+        if (!plugin.getTimer().containsKey(barName)) {
+            MessageUtil.sendMessage(player, "That bar is not active!");
+            return;
+        }
+        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+            bossBar.removeBar(onlinePlayer);
+        }
+        plugin.getTimer().remove(barName);
+        bossBar.setFinished(false);
+        MessageUtil.sendMessage(player, "The bar timer has been stopped!");
+    }
+
+    private void createParameter(Player player, Integer argsLength, String[] args)
+    {
+        if (argsLength <= 4) {
+            MessageUtil.sendMessage(player, "Wrong usage! Use: &a/bbt create <name> <time> <color> <style> <DisplayName>");
+            return;
+        }
+        String barName = args[1];
+        String time = args[2];
+        String color = args[3];
+        String style = args[4];
+        StringBuilder displayName = new StringBuilder();
+        for (int i = 5; i < argsLength; i++) {
+            displayName.append(args[i] + " ");
+        }
+        plugin.getConfig().set("Bars." + barName + ".DisplayName", displayName.toString().trim());
+        plugin.getConfig().set("Bars." + barName + ".Time", time);
+        plugin.getConfig().set("Bars." + barName + ".Color", color.toUpperCase());
+        plugin.getConfig().set("Bars." + barName + ".Style", style.toUpperCase());
+        plugin.saveConfig();
+        plugin.getBarManagerMap().put(barName, plugin.getBarManager());
+        MessageUtil.sendMessage(player, "A new bar timer has been created!");
+    }
+
+    private void startParameter(Player player, Integer argsLength, String[] args)
+    {
+        if (argsLength == 1) {
+            MessageUtil.sendMessage(player, "You need to specify a bar!");
+            return;
+        }
+
+        String barName = args[1];
+
+        if (!plugin.getConfig().isConfigurationSection("Bars." + barName)) {
+            MessageUtil.sendMessage(player, "That bar is not available try another one!");
+            return;
+        }
+        if (plugin.getTimer().containsKey(barName)) {
+            MessageUtil.sendMessage(player, "That bar is already active!");
+            return;
+        }
+        String timeFormat = plugin.getConfig().getString("Bars." + barName + ".Time");
+        plugin.getUtilities().formatTime(barName, timeFormat);
+        BossBarManager bossBar = plugin.getBarManagerMap().get(barName);
+
+        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+            bossBar.addPlayer(onlinePlayer);
+        }
+
+        bossBar.setBarColor(plugin.getConfig().getString("Bars." + barName + ".Color").toUpperCase());
+        bossBar.setBarStyle(plugin.getConfig().getString("Bars." + barName + ".Style").toUpperCase());
+        bossBar.setFinished(false);
+
+        MessageUtil.sendMessage(player, "The bar &e" + barName + " &7has been started!");
+        plugin.getUtilities().animateText(plugin.getConfig().getStringList("Bars." + barName + ".DisplayName.Frames"),
+        plugin.getConfig().getLong("Bars." + barName + ".DisplayName.Period"), bossBar);
+    }
 }
---
+++ src/com/minestom/Utils/MessageUtil.java
@@ -0,0 +1,46 @@
+package com.minestom.Utils;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+public class MessageUtil
+{
+    private static String prefix = "&cBossBarTimer > &7";
+    private static char colorChar = '&';
+
+    public static void sendMessage(Player player, String message)
+    {
+        sendMessage(player, message, true);
+    }
+
+    public static void sendMessage(Player player, String message, boolean usePrefix)
+    {
+        String fullMessage = "";
+
+        if(usePrefix)
+        {
+            fullMessage += prefix;
+        }
+
+        fullMessage += message;
+
+        player.sendMessage(colorMessage(fullMessage));
+    }
+
+    public static void sendMessages(Player player, String[] messages)
+    {
+        sendMessages(player, messages, true);
+    }
+
+    public static void sendMessages(Player player, String[] messages, boolean usePrefix)
+    {
+        for (String message: messages)
+        {
+            sendMessage(player, message, usePrefix);
+        }
+    }
+
+    private static String colorMessage(String text) {
+        return ChatColor.translateAlternateColorCodes(colorChar, text);
+    }
+}
